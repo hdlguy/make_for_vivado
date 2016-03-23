2db24b6e1547c0eeb5d2e7f48127db994d4408b7
pedro <hdlguy@hotmail.com>
2016-03-22 21:42:55 -0600

    adding main tcl compile script for make

---
+++ build/vcomp.tcl
@@ -0,0 +1,64 @@
+# vcomp.tcl
+# This TCL script is intended to be called by gnu make as part of a build system for the Vivado compiler collection.
+# The first argument is the target file. The rest of the arguments are interpreted as input source files.
+if { $::argc > 1 } {
+    create_project -in_memory -part xc7a15tcpg236-2 proj
+    # get the target file
+    set target [lindex $argv 0]
+    set target_type [file extension $target]
+    puts "targetfile = $target"
+    # get the prerequisite files
+    for {set i 1} {$i<$argc} {incr i} {
+        set prereq [lindex $argv $i]
+        puts "prerequisite file = $prereq"
+        set prereq_type [file extension $prereq]
+        puts "prereq type = $prereq_type"
+        switch $prereq_type {
+            .v {
+                puts "verilog filetype detected"
+                read_verilog -sv $prereq
+            }
+            .vhd {
+                puts "VHDL filetype detected"
+                read_vhdl $prereq
+            }
+            .vhdl {
+                puts "VHDL filetype detected"
+                read_vhdl $prereq
+            }
+            .dcp {
+                puts "presynthesized checkpoint filetype detected"
+                read_checkpoint $prereq
+            }
+            .xdc {
+                puts "presynthesized checkpoint filetype detected"
+                read_xdc $prereq
+            }
+            default {
+                puts "WARNING: Could not determine file type."
+            }
+        }
+    }
+    # get the top entity name from the target.
+    set top [file rootname [file tail $target]]
+    synth_design -top $top 
+    if {$target_type == ".dcp"} {
+        write_checkpoint -force $target
+    }
+    if {$target_type == ".bit"} {
+        write_checkpoint -force [file rootname $target]_post_synth.dcp
+        opt_design;
+        place_design;
+        phys_opt_design;
+        route_design;
+        write_checkpoint -force [file rootname $target]_post_route.dcp
+        write_bitstream -verbose -force $target
+    }
+    close_project
+} else {
+    puts "This script converts source files (.dcp, .v, .vhd, .vhdl) to a target .dcp file."
+    puts "USAGE: source hdl2dcp.tcl <path/target.dcp> <path/source.x> <path/source.x> <path/source.x> <path/source.x> ... "
+}
+
+
+
